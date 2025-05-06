741b84b4a29fa594dfd566703cbf65e70e95350e
ferrykurniawan28 <147892622+ferrykurniawan28@users.noreply.github.com>
2025-05-06 22:38:37 +0700

    [UPDATE] bisnis login

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,1 +1,4 @@
-
+const String kBaseUrl = 'https://apidev-triana.sportsnow.app';
+const String kRegisterUrl = '$kBaseUrl/register';
+const String kVerifyOTPUrl = '$kBaseUrl/verify-otp';
+const String kSessionUrl = '$kBaseUrl/session';
